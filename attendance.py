04079dffeb1bda169a4505253372d609f74cd5ae
Deep Thanush <deepthanush07@gmail.com>
2026-06-29 18:43:53 +0530

    Added attendance CSV writing

--- attendance.py
+++ attendance.py
@@ -11,6 +11,9 @@ def menu():
     def mark_attendance():
         name = input("Enter Student Name: ")
     status = input("Enter Attendance(present/Absent): ")
-
+    with open("attendance.csv","a",newline="") as file:
+        writer = csv.writer(file)
+        writer.writerow([name,status])
+        
     
 
